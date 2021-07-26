4b6fbb62b90cb52fce865fa087a34f08a9d0d386
hanseom <hanseomkim@gmail.com>
2021-07-27 08:27:14 +0900

    Case문

--- src/test/java/com/spring/querydsl/QuerydslCaseTest.java
+++ src/test/java/com/spring/querydsl/QuerydslCaseTest.java
@@ -1,10 +1,10 @@
 package com.spring.querydsl;
 
 import com.querydsl.core.Tuple;
-import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.spring.querydsl.entity.Member;
-import com.spring.querydsl.entity.QMember;
 import com.spring.querydsl.entity.Team;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,25 +12,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceUnit;
 import java.util.List;
 
 import static com.spring.querydsl.entity.QMember.member;
-import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
 @Transactional
-public class QuerydslSubQueryTest {
+public class QuerydslCaseTest {
     @PersistenceContext
     EntityManager em;
 
     JPAQueryFactory queryFactory;
-
-    @PersistenceUnit
-    EntityManagerFactory emf;
 
     @BeforeEach
     public void before() {
@@ -51,67 +45,61 @@ public class QuerydslSubQueryTest {
     }
 
     @Test
-    public void subQuery() throws Exception {
-        QMember memberSub = new QMember("memberSub");
-        List<Member> result = queryFactory
-                .selectFrom(member)
-                /** 1) 나이가 가장 많은 회원 조회
-                .where(member.age.eq( JPAExpressions
-                        .select(memberSub.age.max())
-                        .from(memberSub)
-                ))
-                */
-                /** 2) 나이가 평균 나이 이상인 회원
-                .where(member.age.goe(
-                        JPAExpressions
-                                .select(memberSub.age.avg())
-                                .from(memberSub)
-                ))
-                */
-                // 3) in
-                .where(member.age.in(
-                        JPAExpressions
-                                .select(memberSub.age)
-                                .from(memberSub)
-                                .where(memberSub.age.gt(10))
-                ))
-                .fetch();
-        /** 1)
-        assertThat(result).extracting("age")
-                .containsExactly(40);
-         */
-        /** 2)
-        assertThat(result).extracting("age")
-                .containsExactly(30,40);
-        */
-        // 3)
-        assertThat(result).extracting("age")
-                .containsExactly(20, 30, 40);
-
-        // Scala Subquery
-        List<Tuple> fetch = queryFactory
-                .select(member.username,
-                        JPAExpressions
-                                .select(memberSub.age.avg())
-                                .from(memberSub)
-                ).from(member)
+    public void caseTest() throws Exception {
+        List<String> result = queryFactory
+                .select(member.age
+                        .when(10).then("열살")
+                        .when(20).then("스무살")
+                        .otherwise("기타"))
+                .from(member)
                 .fetch();
 
-        for (Tuple tuple : fetch) {
-            System.out.println("username = " + tuple.get(member.username));
-            System.out.println("age = " + tuple.get(JPAExpressions.select(memberSub.age.avg()).from(memberSub)));
+        for (String str : result) {
+            System.out.println("result = " + str);
         }
+    }
 
-        /** static import 활용
-        import static com.querydsl.jpa.JPAExpressions.select;
-
-        List<Member> result = queryFactory
-                .selectFrom(member)
-                .where(member.age.eq(
-                        select(memberSub.age.max())
-                                .from(memberSub)
-                ))
+    @Test
+    public void caseTest2() throws Exception {
+        List<String> result = queryFactory
+                .select(new CaseBuilder()
+                        .when(member.age.between(0, 20)).then("0~20살")
+                        .when(member.age.between(21, 30)).then("21~30살")
+                        .otherwise("기타"))
+                .from(member)
                 .fetch();
+
+        for (String str : result) {
+            System.out.println("result = " + str);
+        }
+    }
+
+    @Test
+    public void caseTest3() throws Exception {
+        /**
+         * orderBy에서 Case문 함께 사용하기
+         * Querydsl은 자바 코드로 작성하기 때문에 rankPath 처럼 복잡한 조건을 변수로 선언해서 select 절, orderBy 절에서 함께 사용할 수 있다.
+         *
+         * 1. 0 ~ 30살이 아닌 회원을 가장 먼저 출력
+         * 2. 0 ~ 20살 회원 출력
+         * 3. 21 ~ 30살 회원 출력
          */
+        NumberExpression<Integer> rankPath = new CaseBuilder()
+                .when(member.age.between(0, 20)).then(2)
+                .when(member.age.between(21, 30)).then(1)
+                .otherwise(3);
+
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age, rankPath)
+                .from(member)
+                .orderBy(rankPath.desc())
+                .fetch();
+
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            Integer rank = tuple.get(rankPath);
+            System.out.println("username = " + username + " age = " + age + " rank = " + rank);
+        }
     }
 }
