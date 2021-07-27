7b71bb595b4739573756ed111e0efe56ec8eae9e
hanseom <hanseomkim@gmail.com>
2021-07-27 09:21:15 +0900

    Querydsl Projection

---
+++ src/main/java/com/spring/querydsl/dto/MemberDto.java
@@ -0,0 +1,15 @@
+package com.spring.querydsl.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberDto {
+    private String username;
+    private int age;
+    public MemberDto() {
+    }
+    public MemberDto(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+}
---
+++ src/test/java/com/spring/querydsl/QuerydslProjectionsTest.java
@@ -0,0 +1,91 @@
+package com.spring.querydsl;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.spring.querydsl.dto.MemberDto;
+import com.spring.querydsl.entity.Member;
+import com.spring.querydsl.entity.Team;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
+import static com.spring.querydsl.entity.QMember.member;
+
+
+@SpringBootTest
+@Transactional
+public class QuerydslProjectionsTest {
+    @PersistenceContext
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void property() throws Exception {
+        List<MemberDto> result = queryFactory
+                .select(Projections.bean(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto.username = " + memberDto.getUsername());
+        }
+    }
+
+    @Test
+    public void field() throws Exception {
+        List<MemberDto> result = queryFactory
+                .select(Projections.fields(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto.username = " + memberDto.getUsername());
+        }
+    }
+
+    @Test
+    public void constructor() throws Exception {
+        List<MemberDto> result = queryFactory
+                .select(Projections.constructor(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member).fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto.username = " + memberDto.getUsername());
+        }
+    }
+}
+
+
+
+
