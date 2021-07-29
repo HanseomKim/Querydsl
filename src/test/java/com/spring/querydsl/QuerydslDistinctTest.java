1888db5a6792703258129564a52b82d8fe2ed682
hanseom <hanseomkim@gmail.com>
2021-07-29 20:19:01 +0900

    querydsl distinct

---
+++ src/test/java/com/spring/querydsl/QuerydslDistinctTest.java
@@ -0,0 +1,59 @@
+package com.spring.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.spring.querydsl.entity.Member;
+import com.spring.querydsl.entity.Team;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+import static com.spring.querydsl.entity.QMember.member;
+
+
+@SpringBootTest
+@Transactional
+public class QuerydslDistinctTest {
+    @PersistenceContext
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member1", 20, teamA);
+        Member member3 = new Member("member2", 30, teamB);
+        Member member4 = new Member("member2", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void distinct() throws Exception {
+        List<String> result = queryFactory
+                .select(member.username).distinct()
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+}
+
+
+
+
